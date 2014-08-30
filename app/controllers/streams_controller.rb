a08273fbc0264e476f651207cbe6ab8e0772dbb6
Chris Hunt <chrishunt@github.com>
2014-08-30 16:52:55 -0700

    Show connection status

--- app/controllers/streams_controller.rb
+++ app/controllers/streams_controller.rb
@@ -12,6 +12,11 @@ class StreamsController < ApplicationController
     redirect_to stream.init!
   end
 
+  def destroy
+    Stream.find_by(slug: params['id']).destroy!
+    redirect_to new_stream_path
+  end
+
   def show
     @stream = Stream.find_by(slug: params['id'])
     @token = OPENTOK.generate_token @stream.opentok_id
